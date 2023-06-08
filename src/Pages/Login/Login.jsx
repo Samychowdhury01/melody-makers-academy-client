0a0d511d50fa8b57a2fcb9af2b18a091458e676e
Samychowdhury01 <samychowdhury84@gmail.com>
2023-06-08 15:46:27 +0600

    added register page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,28 +6,37 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { toast } from "react-hot-toast";
 import { Helmet } from "react-helmet-async";
 import SocialLogin from "../../Components/SocialLogin/SocialLogin";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [showPass, setShowPass] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const {email, password} = data
 
-  const handleLogin = (event) => {};
-
+    setErrorMessage('')
+  };
+console.log(errors)
   return (
     <>
       <Helmet>
         <title> MelodyMakers Academy | Login</title>
       </Helmet>
 
-      <div className="hero min-h-screen form-bg">
-        <div className="hero-content flex-col lg:flex-row gap-10 shadow-2xl">
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row gap-24 shadow-2xl">
           <div className="text-center lg:text-left">
             <img src={loginImg} alt="login-image" />
           </div>
           <div className=" md:w-2/3 bg-[#1f1f1f] rounded-2xl">
-            <form onSubmit={handleLogin} className="card-body">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
               <h1 className="text-center text-4xl font-bold text-white">
                 Login
               </h1>
@@ -38,9 +47,13 @@ const Login = () => {
                 <input
                   type="email"
                   name="email"
+                  {...register("email", { required: true })}
                   placeholder="Email"
                   className="input input-bordered"
                 />
+                {errors.email && (
+                  <span className="text-red-500">This field is required</span>
+                )}
               </div>
               <div className="form-control relative">
                 <label className="label">
@@ -58,10 +71,12 @@ const Login = () => {
                 </div>
                 <input
                   type={showPass ? "text" : "password"}
+                  {...register("password", {
+                    required: true,
+                  })}
                   name="password"
                   placeholder="Password"
                   className="input input-bordered"
-                  required
                 />
                 <label className="text-center mt-3 text-red-600 font-bold">
                   <p>{errorMessage}</p>
@@ -71,21 +86,21 @@ const Login = () => {
                 <input
                   type="submit"
                   value="Login"
-                  className="btn bg-[#86E5DC] text-black border-none"
+                  className="btn bg-[#86E5DC] text-black border-none rounded-3xl"
                 />
               </div>
               <label className="mt-3 text-lg text-gray-400">
-                <span>New here?</span>{" "}
+                <span>New here?  </span>
                 <Link
                   to="/register"
                   state={location?.state}
-                  className="text-[#86E5DC] hover:link link-neutral font-bold duration-300 transition-all"
+                  className="text-[#86E5DC] hover:link link-accent font-bold duration-300 transition-all"
                 >
                   Create a New Account
                 </Link>
               </label>
             </form>
-            <SocialLogin/>
+            <SocialLogin />
           </div>
         </div>
       </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,178 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import loginImg from "../../assets/login/login.svg";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { Helmet } from "react-helmet-async";
+import useAuth from "../../Hooks/useAuth";
 
 const Register = () => {
-    return (
-        <div>
-            
+  const { createUser, updateUserProfile } = useAuth();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showPass, setShowPass] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  //   handler for registration
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const { name, email, password, captcha, photo } = data;
+    setErrorMessage("");
+  };
+
+
+
+  return (
+    <>
+      <Helmet>
+        <title> MelodyMakers Academy | Register</title>
+      </Helmet>
+
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-24 shadow-2xl">
+          <div className="text-center lg:text-left">
+            <img src={loginImg} alt="login-image" />
+          </div>
+          <div className="md:w-2/3 bg-[#1f1f1f] rounded-2xl">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <h1 className="text-center text-4xl font-bold text-white">Sign Up</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-gray-400">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  name="name"
+                  placeholder="Name"
+                  className="input input-bordered"
+                />
+                {errors.name && (
+                  <span className="text-red-500">This field is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-gray-400">Photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("photo", { required: true })}
+                  name="photo"
+                  placeholder="Photo URL"
+                  className="input input-bordered"
+                />
+                {errors.photo && (
+                  <span className="text-red-500">This field is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-gray-400">Email</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  name="email"
+                  placeholder="Email"
+                  className="input input-bordered"
+                />
+                {errors.email && (
+                  <span className="text-red-500">Email is required</span>
+                )}
+              </div>
+              <div className="form-control relative">
+                <label className="label">
+                  <span className="label-text text-gray-400">Password</span>
+                </label>
+                <div
+                  onClick={() => setShowPass(!showPass)}
+                  className="cursor-pointer absolute right-4 top-12 "
+                >
+                  {showPass ? (
+                    <FaEye className="text-2xl" />
+                  ) : (
+                    <FaEyeSlash className="text-2xl" />
+                  )}
+                </div>
+                <input
+                  type={showPass ? "text" : "password"}
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
+                  name="password"
+                  placeholder="Password"
+                  className="input input-bordered"
+                />
+                {errors.password?.type === "required" && (
+                  <p className="text-red-600">Password is required</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-600">Password must be 6 characters</p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p className="text-red-600">
+                    Password must be less than 20 characters
+                  </p>
+                )}
+                {errors.password?.type === "pattern" && (
+                  <p className="text-red-600">
+                    Password must have one Uppercase <br /> one lower case, one
+                    number and one special character.
+                  </p>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-gray-400">Confirm Password</span>
+                </label>
+                <input
+                  type={showPass ? "text" : "password"}
+                  {...register("confirmPassword", {
+                    required: true,
+                  })}
+                  placeholder="Confirm Password"
+                  className="input input-bordered"
+                />
+                {errors.confirmPassword && (
+                  <span className="text-red-500">this field is required</span>
+                )}
+                <label className="text-center mt-3 text-red-600 font-bold">
+                  <p>{errorMessage}</p>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Sign Up"
+                  className="btn bg-[#86E5DC] text-black border-none rounded-3xl"
+                />
+              </div>
+              <label className="mt-3 text-lg text-gray-400">
+                <span>Already registered? </span>
+                <Link
+                  to="/login"
+                  state={location?.state}
+                  className="text-[#86E5DC] hover:link link-accent font-bold duration-300 transition-all"
+                >
+                  Go to log in
+                </Link>
+              </label>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Register;
